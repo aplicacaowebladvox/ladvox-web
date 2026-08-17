2a8cabd695479b9b0215eede5280069568faf6e4
Marlan Jovencio <marlan.jovencio@itixti.com.br>
2026-08-17 14:50:22 -0300

    build: ajustando environment.prd

--- src/environments/environment.prd.ts
+++ src/environments/environment.prd.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   name: 'PRODUCTION',
   config: {
-    apiUrl: 'http://ladvox-lb-1266238589.us-east-1.elb.amazonaws.com/projeto-fono/api/',
+    apiUrl: 'https://ladvox-core.onrender.com/projeto-fono/api/',
   },
 };
